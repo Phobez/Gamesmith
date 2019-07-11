5004b485bcb576207536d3bb4b7437cad61464f4
Phobez <abiaph@live.com>
2019-07-11 21:45:47 +0700

    Implemented UI classes.
    created LoadSceneOnClick, LoadSceneOnKeyDown, VolumeSetter, QuitGame Scripts
    
    Co-Authored-By: hansbudiman <hansbudiman@users.noreply.github.com>

---
+++ Assets/Scripts/UI/LoadSceneOnKeyDown.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Written by:  Hans Budiman
+
+public class LoadSceneOnKeyDown : MonoBehaviour
+{
+
+    public string targetScene;
+    public KeyCode key;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/VolumeSetter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+// Written by:  Hans Budiman
+public class VolumeSetter : MonoBehaviour
+{
+    public AudioMixer am;
+    public Slider s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Volume") == false)
+        {
+            PlayerPrefs.SetFloat("Volume", 0.0f);
+        }
+    }
+
+    private void OnEnable()
+    {
+        s.value = Mathf.Pow(10, PlayerPrefs.GetFloat("Volume") / 20);
+    }
+
+    public void SetVolume()
+    {
+        float value = Mathf.Log10(s.value) * 20;
+        am.SetFloat("Volume", value);
+        PlayerPrefs.SetFloat("Volume", value);
+    }
+}
